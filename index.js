b7119547aabd5b8edb3086f4585b54d66300c0f5
Nakamura Yuto <61489632+yutaro049@users.noreply.github.com>
2021-09-05 13:12:49 +0900

    commit

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ mongoose
   .then(console.log("connected"))
   .catch((err) => console.log(err));
 
+app.use(express.json());
+app.use("/api/posts", postRouter);
+
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
-
-app.use(express.json());
-app.use("/api/posts", postRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("backend is running");
