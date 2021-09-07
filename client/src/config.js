92d5272c7e03ee86e76c48c9619e7fffdc1b4fe2
Nakamura Yuto <61489632+yutaro049@users.noreply.github.com>
2021-09-07 22:54:01 +0900

    commit

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://nakamurayuto.herokuapp.com/api/",
+  baseURL: "https://yutonakamura.herokuapp.com/api/",
 });
